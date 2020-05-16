6b6829661df49b4e03cd4ea3a1aad8ddb5ca6019
Bree Warren <ambreea.warren@gmail.com>
2020-05-16 17:55:03 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -61,6 +61,8 @@ def add_characters
    #   - The second hash should have the :name key set to "Apothecary" 
    # Code your solution here:
 
+add_characters[epic_tragedy] = {:montague}, {
+:additional_characters
 
 
 
